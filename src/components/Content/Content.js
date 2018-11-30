b4cf5a39a202aca8bf64632a9c4740a793639173
adanli <adanli@126.com>
2018-11-30 16:16:56 +0800

    '账号退出'

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -2,12 +2,18 @@ import React, {Component} from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom';
 
 import {connect} from 'react-redux';
+import {withRouter} from 'react-router-dom';
+
 import {toggleCollapsed} from '../../redux/actions/app';
 
 import Bundle from '../../router/Bundle';
 import {Layout, Icon, Breadcrumb} from 'antd';
 
+import axios from 'axios';
+
 import './Content.css';
+import 'antd/dist/antd.css';
+
 
 const Loading = function () {
     return <div>Loading...</div>
@@ -23,9 +29,19 @@ const createComponent = (component) => (props) => (
 
 
 class Content extends Component{
+
+    logout = () => {
+        axios.get("/api/loginController/logout")
+        // axios.get("/api/loginController/logout")
+            .then(r => {
+                this.props.history.push("/user/login")
+            })
+
+
+    }
     
     render(){
-        let a = require('bundle-loader?lazy&name=[name]!pages/JobManager/JobManager')
+
         return (
             <Layout>
                 <Layout.Header style={{ background: '#fff', padding: 0 }}>
@@ -33,6 +49,12 @@ class Content extends Component{
                         className="trigger"
                         type={this.props.app.collapsed ? 'menu-unfold' : 'menu-fold'}
                         onClick={() => this.props.toggleCollapsed()}
+                    />
+                    <Icon
+                        className="trigger"
+                        type="logout"
+                        style={{float: "right"}}
+                        onClick={this.logout}
                     />
                 </Layout.Header>
                 <Breadcrumb separator=">" style={{padding: "15px"}}>
@@ -54,4 +76,4 @@ class Content extends Component{
     }
 }
 
-export default connect((state) => ({app: state.app}), {toggleCollapsed})(Content)
+export default withRouter(connect((state) => ({app: state.app}), {toggleCollapsed})(Content))
